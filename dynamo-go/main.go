fb1da6bf51bde9f41bd07defab4bc3d994344479
nirmash <nirmsk@hotmail.com>
2020-08-10 10:49:18 -0700

    Initial commit

---
+++ dynamo-go/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
+
+type iEvent struct {
+	Id       string `json:"id"`
+	Obj_Name string `json:"obj_name"`
+	Body     string `json:"body"`
+}
+
+func HandleRequest(ctx context.Context, body iEvent) (string, error) {
+	fmt.Println(body.Body)
+	fmt.Println(body.Id)
+	SaveToDynamo(body)
+	return fmt.Sprintf(body.Id), nil
+}
+
+func SaveToDynamo(msg iEvent) {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-west-2")},
+	)
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
+	av, err := dynamodbattribute.MarshalMap(msg)
+
+	if err != nil {
+		fmt.Println("Got error marshalling map:")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(os.Getenv("DYNAMO_TABLE_NAME")),
+	}
+
+	_, err = svc.PutItem(input)
+
+	if err != nil {
+		fmt.Println("Got error calling PutItem:")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
